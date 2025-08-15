5f974d4eda657a0c9b367d999a552b36a9607115
Devin Alexander <devinmalexander@gmail.com>
2025-08-15 13:41:04 -0500

    Completed "Implement Async Action"

--- Source/FrontendUI/Private/AsyncActions/AsyncAction_PushSoftWidget.cpp
+++ Source/FrontendUI/Private/AsyncActions/AsyncAction_PushSoftWidget.cpp
@@ -2,10 +2,10 @@
 
 
 #include "AsyncActions/AsyncAction_PushSoftWidget.h"
+#include "Subsystems/FrontendUISubsystem.h"
+#include "Widgets/Widget_ActivatableBase.h"
 
-UAsyncAction_PushSoftWidget * UAsyncAction_PushSoftWidget::PushSoftWidget(const UObject * WorldContextObject, 
-    APlayerController * OwningPlayerController, TSoftClassPtr<UWidget_ActivatableBase> InSoftWidgetClass, 
-    UPARAM(meta = (Categories = "Frontend.WidgetStack")) FGameplayTag InWidgetStackTag, bool bFocusOnNewlyPushedWidget)
+UAsyncAction_PushSoftWidget * UAsyncAction_PushSoftWidget::PushSoftWidget(const UObject * WorldContextObject, APlayerController * OwningPlayerController, TSoftClassPtr<UWidget_ActivatableBase> InSoftWidgetClass, UPARAM(meta = (Categories = "Frontend.WidgetStack")) FGameplayTag InWidgetStackTag, bool bFocusOnNewlyPushedWidget)
 {
     checkf(!InSoftWidgetClass.IsNull(),TEXT("PushSoftWidgetToStack was passed a null soft widget class "));
 
@@ -14,6 +14,11 @@ UAsyncAction_PushSoftWidget * UAsyncAction_PushSoftWidget::PushSoftWidget(const 
         if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject,EGetWorldErrorMode::LogAndReturnNull))
         {
             UAsyncAction_PushSoftWidget* Node = NewObject<UAsyncAction_PushSoftWidget>();
+            Node->CachedOwningWorld = World;
+            Node->CachedOwningPC = OwningPlayerController;
+            Node->CachedSoftWidgetClass = InSoftWidgetClass;
+            Node->CachedWidgetStackTag = InWidgetStackTag;
+            Node->bCachedFocusOnNewlyPushedWidget = bFocusOnNewlyPushedWidget;
 
             Node->RegisterWithGameInstance(World);
 
@@ -22,4 +27,43 @@ UAsyncAction_PushSoftWidget * UAsyncAction_PushSoftWidget::PushSoftWidget(const 
     }
 
     return nullptr;
+}
+
+void UAsyncAction_PushSoftWidget::Activate()
+{
+    UFrontendUISubsystem* FrontendUISubsystem = UFrontendUISubsystem::Get(CachedOwningWorld.Get());
+
+    FrontendUISubsystem->PushSoftWidgetToStackAsync(CachedWidgetStackTag,CachedSoftWidgetClass,
+        [this](EAsyncPushWidgetState InPushState, UWidget_ActivatableBase* PushedWidget)
+        {
+            switch (InPushState)
+            {
+            case EAsyncPushWidgetState::OnCreatedBeforePush:
+
+                PushedWidget->SetOwningPlayer(CachedOwningPC.Get());
+
+                OnWidgetCreatedBeforePush.Broadcast(PushedWidget);
+
+                break;
+
+            case EAsyncPushWidgetState::AfterPush:
+
+                AfterPush.Broadcast(PushedWidget);
+
+                if (bCachedFocusOnNewlyPushedWidget)
+                {
+                    if (UWidget* WidgetToFocus = PushedWidget->GetDesiredFocusTarget())
+                    {
+                        WidgetToFocus->SetFocus();
+                    }
+                }
+
+                SetReadyToDestroy();
+
+                break;
+            default:
+                break;
+            }
+        }
+    );
 }
