b517e497473218b6f338aae49ea67cb5ba7a78da
Devin Alexander <devinmalexander@gmail.com>
2025-08-28 16:48:19 -0500

    Completed "On Button Clicked"

--- Source/FrontendUI/Private/Widgets/Options/ListEntries/Widget_ListEntry_String.cpp
+++ Source/FrontendUI/Private/Widgets/Options/ListEntries/Widget_ListEntry_String.cpp
@@ -4,8 +4,19 @@
 #include "Widgets/Options/ListEntries/Widget_ListEntry_String.h"
 #include "Widgets/Options/DataObjects/ListDataObject_String.h"
 #include "Widgets/Components/FrontendCommonRotator.h"
+#include "Widgets/Components/FrontendCommonButtonBase.h"
 
-void UWidget_ListEntry_String::OnOwningListDataObjectSet(UListDataObject_Base* InOwningListDataObject)
+#include "FrontendDebugHelper.h"
+
+void UWidget_ListEntry_String::NativeOnInitialized()
+{
+    Super::NativeOnInitialized();
+
+    CommonButton_PreviousOption->OnClicked().AddUObject(this,&ThisClass::OnPreviousOptionButtonClicked);
+    CommonButton_NextOption->OnClicked().AddUObject(this,&ThisClass::OnNextOptionButtonClicked);
+}
+
+void UWidget_ListEntry_String::OnOwningListDataObjectSet(UListDataObject_Base *InOwningListDataObject)
 {
     Super::OnOwningListDataObjectSet(InOwningListDataObject);
 
@@ -13,5 +24,14 @@ void UWidget_ListEntry_String::OnOwningListDataObjectSet(UListDataObject_Base* I
 
     CommonRotator_AvailableOptions->PopulateTextLabels(CachedOwningStringDataObject->GetAvailableOptionsTextArray());
     CommonRotator_AvailableOptions->SetSelectedOptionByText(CachedOwningStringDataObject->GetCurrentDisplayText());
-    
+}
+
+void UWidget_ListEntry_String::OnPreviousOptionButtonClicked()
+{
+    Debug::Print(TEXT("Previous Option Button Clicked"));
+}
+
+void UWidget_ListEntry_String::OnNextOptionButtonClicked()
+{
+    Debug::Print(TEXT("Next Option Button Clicked"));
 }
