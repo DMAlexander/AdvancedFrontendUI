5a6a4dd65322c71c9ce55c464f58088e8c375ee0
Devin Alexander <devinmalexander@gmail.com>
2025-08-28 17:54:01 -0500

    Completed "Data Dynamic Setter/Getter"

--- Source/FrontendUI/Public/Widgets/Options/DataObjects/ListDataObject_Value.h
+++ Source/FrontendUI/Public/Widgets/Options/DataObjects/ListDataObject_Value.h
@@ -6,12 +6,20 @@
 #include "Widgets/Options/DataObjects/ListDataObject_Base.h"
 #include "ListDataObject_Value.generated.h"
 
+class FOptionsDataInteractionHelper;
 /**
  * 
  */
-UCLASS()
+UCLASS(Abstract)
 class FRONTENDUI_API UListDataObject_Value : public UListDataObject_Base
 {
 	GENERATED_BODY(Abstract)
-	
+
+public:
+	void SetDataDynamicGetter(const TSharedPtr<FOptionsDataInteractionHelper>& InDynamicGetter);
+	void SetDataDynamicSetter(const TSharedPtr<FOptionsDataInteractionHelper>& InDynamicSetter);
+
+protected:
+	TSharedPtr<FOptionsDataInteractionHelper> DataDynamicGetter;
+	TSharedPtr<FOptionsDataInteractionHelper> DataDynamicSetter;
 };
