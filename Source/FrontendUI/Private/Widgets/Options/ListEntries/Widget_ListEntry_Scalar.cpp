59f5dfb78d2a0b973fbfe5ca87122eaa7c2deb8f
Devin Alexander <devinmalexander@gmail.com>
2025-08-30 18:14:27 -0500

    Completed "Scalar Entry Widget"

---
+++ Source/FrontendUI/Private/Widgets/Options/ListEntries/Widget_ListEntry_Scalar.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/Options/ListEntries/Widget_ListEntry_Scalar.h"
+
+void UWidget_ListEntry_Scalar::NativeOnInitialized()
+{
+    Super::NativeOnInitialized();
+}
+
+void UWidget_ListEntry_Scalar::OnOwningListDataObjectSet(UListDataObject_Base *InOwningListDataObject)
+{
+    Super::OnOwningListDataObjectSet(InOwningListDataObject);
+}
+
+void UWidget_ListEntry_Scalar::OnOwningListDataObjectModified(UListDataObject_Base *OwningModifiedData, EOptionsListDataModifyReason ModifyReason)
+{
+}
