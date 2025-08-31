be768bd0672c20917fce0b7f4f23e336bb823656
Devin Alexander <devinmalexander@gmail.com>
2025-08-31 07:27:28 -0500

    Completed "Volume Config Variables"

--- Source/FrontendUI/Private/FrontendSettings/FrontendGameUserSettings.cpp
+++ Source/FrontendUI/Private/FrontendSettings/FrontendGameUserSettings.cpp
@@ -3,6 +3,10 @@
 
 #include "FrontendSettings/FrontendGameUserSettings.h"
 
+UFrontendGameUserSettings::UFrontendGameUserSettings(): OverallVolume(1.f)
+{
+}
+
 UFrontendGameUserSettings* UFrontendGameUserSettings::Get()
 {
     if (GEngine)
@@ -11,4 +15,11 @@ UFrontendGameUserSettings* UFrontendGameUserSettings::Get()
     }
 
     return nullptr;
+}
+
+void UFrontendGameUserSettings::SetOverallVolume(float InVolume)
+{
+    OverallVolume = InVolume;
+
+    //The actual logic for controlling the volume goes here
 }
