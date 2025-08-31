54b9d94f5e30096afc8afac23c494486936647e0
Devin Alexander <devinmalexander@gmail.com>
2025-08-31 08:31:27 -0500

    Completed "Construct String Bool"

--- Source/FrontendUI/Private/FrontendSettings/FrontendGameUserSettings.cpp
+++ Source/FrontendUI/Private/FrontendSettings/FrontendGameUserSettings.cpp
@@ -3,7 +3,7 @@
 
 #include "FrontendSettings/FrontendGameUserSettings.h"
 
-UFrontendGameUserSettings::UFrontendGameUserSettings(): OverallVolume(1.f), MusicVolume(1.f), SoundFXVolume(1.f)
+UFrontendGameUserSettings::UFrontendGameUserSettings(): OverallVolume(1.f), MusicVolume(1.f), SoundFXVolume(1.f), bAllowBackgroundAudio(false)
 {
 }
 
@@ -31,4 +31,9 @@ void UFrontendGameUserSettings::SetMusicVolume(float InVolume)
 void UFrontendGameUserSettings::SetSoundFXVolume(float InVolume)
 {
     SoundFXVolume = InVolume;
+}
+
+void UFrontendGameUserSettings::SetAllowBackgroundAudio(bool bIsAllowed)
+{
+    bAllowBackgroundAudio = bIsAllowed;
 }
